4e5f22087158086daff5a944be13bb7eb7d9ea9f
Qusai Safa <153466377+QusaiBSafa@users.noreply.github.com>
2026-07-10 03:00:36 +0300

    Add src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { router } from './routes/api';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use('/api', router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
